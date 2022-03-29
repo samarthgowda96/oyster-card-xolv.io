f631e9fbf892983114db58654f5e843ae873ada9
Samarth Gowda <samarthgowda13@gmail.com>
2022-03-29 16:57:15 -0400

    created-test-resolver

--- index.ts
+++ index.ts
@@ -1,40 +1,37 @@
 import dotenv from "dotenv";
 dotenv.config();
+import "reflect-metadata";
 import { ApolloServer } from "apollo-server-express"
-import { createConnection } from "typeorm";
-import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import {
+    ApolloServerPluginLandingPageGraphQLPlayground,
+    ApolloServerPluginLandingPageProductionDefault,
+  } from "apollo-server-core";
 import  {buildSchema} from  "type-graphql"
-import Express from 'express';
+import express from 'express';
 import 'reflect-metadata';
 import {resolvers} from "./resolvers"
-
 import {connect} from 'mongoose';
-import { Plugins } from "@typegoose/typegoose";
 
-const app = Express();
-
-const mangoose =  connect(`mongodb+srv://${process.env.USER}:${process.env.DB_PASSWORD}@cluster0.jippb.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
-async () =>{
+(async () =>{
+    const mangoose =  connect(`mongodb+srv://${process.env.USER}:${process.env.DB_PASSWORD}@cluster0.jippb.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
     const schema = await buildSchema({
         resolvers,
     })
-    const app = Express();
+    const app = express();
     const server = new ApolloServer({
-        schema
-        
-        
-      });
-      await server.start();
-      server.applyMiddleware({app, cors:false});
+        schema,
+    })
+      await server.start()
+      server.applyMiddleware({app});
 
      
     //app.use(cookieParser())
+    app.listen({ port: 4000 }, () => {
+        console.log("App is listening on http://localhost:4000");
+      });
     
-}
+})();
 
 
 
  
-app.listen(7000, ()=>{
-    console.log("listening on port 7000")
-})
