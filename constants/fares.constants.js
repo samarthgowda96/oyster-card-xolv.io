43b5d9097ce6f2ec1c0ada6de1a25c7935d09df4
Samarth Gowda <samarthgowda13@gmail.com>
2022-03-30 11:20:01 -0400

    defined the consts

---
+++ constants/fares.constants.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.fares = void 0;
+exports.fares = {
+    MAX_FARE: 3.20,
+    ANYWHERE_IN_ZONE1: 2.50,
+    ONE_ZONE_OUTSIDE_ZONE1: 2.00,
+    TWO_ZONE_INCLUDING_ZONE1: 3.00,
+    TWO_ZONE_EXCLUDING_ZONE1: 2.25,
+    THREE_ZONES: 3.20,
+    ANY_BUS_TRIP: 1.80,
+};
+;
---
+++ constants/transport.constants.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ModeOfTransport = void 0;
+exports.ModeOfTransport = {
+    BUS: "BUS",
+    TUBE: "TUBE"
+};
